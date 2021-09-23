237a76e325fa2ad12408aeeef955b190da2417fd
Prashant Sudeep <prashant@thb.co.in>
2021-09-23 09:40:39 +0530

    package json changed

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
+declare const module: any;
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+  console.log(`Application is running on: ${await app.getUrl()}`);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
